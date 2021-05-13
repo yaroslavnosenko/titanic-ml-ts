bb8f9fb9a202f176a45cd0e8984fbff05c75d62f
Yaroslav Nosenko <yaroslav.nosenko.mail@gmail.com>
2021-05-13 13:28:21 +0300

    added cors

--- server/app.ts
+++ server/app.ts
@@ -1,11 +1,13 @@
 import * as express from 'express'
 import { Response, Application } from 'express'
 import { routes } from './routes'
+import * as cors from 'cors'
 
 const app: Application = express()
 
-app.get('/', (_, res: Response) => res.sendStatus(200))
 app.use(express.json())
+app.use(cors())
 app.use(routes)
+app.get('/', (_, res: Response) => res.sendStatus(200))
 
 export default app
