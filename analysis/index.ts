f5d48731dc67f64106bee6283e54584446ec5a45
Yaroslav Nosenko <yaroslav.nosenko.mail@gmail.com>
2021-05-13 08:11:50 +0300

    prettier fix

--- analysis/index.ts
+++ analysis/index.ts
@@ -12,33 +12,32 @@ console.table({ size: data.length })
 
 // Missing values
 
-const missing = Object.keys(data[0]).map((colName) => [
+const missing = Object.keys(data[0]).map(colName => [
   colName,
-  data.filter((row) => row[colName] === '').length,
+  data.filter(row => row[colName] === '').length,
 ])
 
 console.log('\nMissing Values:')
 console.table(missing)
 
 const ageCorrelation = sampleCorrelation(
-  data.map((row) => (row['Age'] === '' ? 0 : parseFloat(row['Age']))),
-  data.map((row) => parseInt(row['Survived']))
+  data.map(row => (row['Age'] === '' ? 0 : parseFloat(row['Age']))),
+  data.map(row => parseInt(row['Survived']))
 )
 console.table({ 'Age Correlation': ageCorrelation })
 
 console.log('\nEmbarked Unique Values:')
 console.table(getUnique(data, 'Embarked'))
 
-const embarkedMode = getMode(data.map((row) => row['Embarked']))
-data = data.map((row) => {
+const embarkedMode = getMode(data.map(row => row['Embarked']))
+data = data.map(row => {
   const newRow = { ...row }
   delete newRow['PassengerId']
   delete newRow['Age']
   delete newRow['Cabin']
   delete newRow['Name']
   delete newRow['Ticket']
-  newRow['Embarked'] =
-    newRow['Embarked'] === '' ? embarkedMode : newRow['Embarked']
+  newRow['Embarked'] = newRow['Embarked'] === '' ? embarkedMode : newRow['Embarked']
   newRow['Survived'] = parseFloat(newRow['Survived'])
   newRow['Pclass'] = parseFloat(newRow['Pclass'])
   newRow['SibSp'] = parseFloat(newRow['SibSp'])
@@ -51,8 +50,8 @@ data = data.map((row) => {
 
 const strCols = ['Sex', 'Embarked']
 const strConf = []
-strCols.forEach((colName) => {
-  const vals = data.map((row) => row[colName])
+strCols.forEach(colName => {
+  const vals = data.map(row => row[colName])
   const [mapped, config] = strToIndex(vals)
   data = data.map((row, index) => ({ ...row, [colName]: mapped[index] }))
   strConf.push({ col: colName, config: config })
@@ -63,8 +62,8 @@ console.table(strConf)
 
 const numCols = ['Pclass', 'SibSp', 'Parch', 'Fare', 'Sex', 'Embarked']
 const scalerConf = []
-numCols.forEach((colName) => {
-  const vals = data.map((row) => row[colName])
+numCols.forEach(colName => {
+  const vals = data.map(row => row[colName])
   const [scaled, min, max] = minMax(vals)
   data = data.map((row, index) => ({ ...row, [colName]: scaled[index] }))
   scalerConf.push({ col: colName, min: min, max: max })
@@ -76,15 +75,8 @@ console.table(scalerConf)
 console.log('\nFirst Row:')
 console.table(data[0])
 
-const preparedData = data.map((row) => ({
-  input: [
-    row['Pclass'],
-    row['Sex'],
-    row['SibSp'],
-    row['Parch'],
-    row['Fare'],
-    row['Embarked'],
-  ],
+const preparedData = data.map(row => ({
+  input: [row['Pclass'], row['Sex'], row['SibSp'], row['Parch'], row['Fare'], row['Embarked']],
   output: [row['Survived']],
 }))
 const [train, test] = split(preparedData, 0.6, true)
